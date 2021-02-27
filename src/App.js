8fcdb018fc9130d41e7638215ecebc85df69e404
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-02-28 02:44:48 +0530

    Ability and Type category working with options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
 import { useState, useEffect } from 'react';
+import CategoryType from './CategoryType';
 import PokemonCard from './PokemonCard';
 
 function App() {
+	let [fetchURL, updateURL] = useState('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
 	let [List, updateList] = useState();
+	let [category, updateCategory] = useState('all');
+	let [categoryOptions, setCategoryOptions] = useState('Select an option');
+	let [optionsURL, setOptionsURL] = useState();
+	let [cardRenderURL, setCardRenderURL] = useState();
+
+	function handleCategory(val) {
+		updateCategory(val.target.value);
+
+		switch (val.target.value) {
+			case 'all':
+				updateURL('https://pokeapi.co/api/v2/pokemon?limit=100&offset=0');
+				break;
+
+			case 'type':
+				updateURL('https://pokeapi.co/api/v2/type/');
+				break;
+
+			case 'habitat':
+				updateURL('https://pokeapi.co/api/v2/pokemon-habitat/');
+				break;
+
+			case 'ability':
+				updateURL('https://pokeapi.co/api/v2/ability/');
+				break;
+
+			default:
+				alert('Error !!!!!');
+				break;
+		}
+	}
+
+	function handleOptions(action) {
+		optionsURL.forEach(el => {
+			if (el.name === action.target.value) {
+				setCardRenderURL(el.url);
+			}
+		});
+	}
 
 	useEffect(() => {
-		fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
+		fetch(fetchURL)
 			.then(res => res.json())
 			.then(data => {
-				//console.log(data.results.map(el => console.log(el.name)));
-				//updateList(data.results.map(el => <h3>{el}</h3>));
-				updateList(data.results.map(el => <PokemonCard key={el.name} Name={el.name} infoURL={el.url} />));
+				if (category !== 'all') {
+					setOptionsURL(data.results);
+					setCategoryOptions(
+						data.results.map(el => (
+							<option key={el.name} value={el.name}>
+								{el.name}
+							</option>
+						))
+					);
+					console.log(data);
+					//updateList(data.pokemon.map(el => <PokemonCard key={el.pokemon.name} Name={el.pokemon.name} infoURL={el.pokemon.url} />));
+				} else {
+					setCategoryOptions(<option>Select a category</option>);
+					updateList(data.results.map(el => <PokemonCard key={el.name} Name={el.name} infoURL={el.url} />));
+				}
 			})
 			.catch(console.log);
-	}, []);
+	}, [fetchURL]);
+
+	useEffect(() => {
+		fetch(cardRenderURL)
+			.then(res => res.json())
+			.then(data => {
+				switch (category) {
+					case 'type':
+						updateList(data.pokemon.map(el => <PokemonCard key={el.pokemon.name} Name={el.pokemon.name} infoURL={el.pokemon.url} />));
+						break;
+
+					case 'habitat':
+						console.log(data.pokemon_species[0]);
+						//updateList(data.pokemon_species.map(el => <PokemonCard key={el.name} Name={el.name} infoURL={el.url} />));
+						break;
+
+					case 'ability':
+						updateList(data.pokemon.map(el => <PokemonCard key={el.pokemon.name} Name={el.pokemon.name} infoURL={el.pokemon.url} />));
+						break;
+
+					default:
+						break;
+				}
+			})
+			.catch(console.log);
+	}, [cardRenderURL]);
 
 	return (
 		<div className='App'>
 			<div className='title-container'>
 				<img src='https://cdn.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png' alt='' />
 			</div>
+			<CategoryType handleCategory={handleCategory} />
+			<select name='category' id='category-menu' onChange={handleOptions}>
+				{categoryOptions}
+			</select>
 			<div className='card-container'>{List}</div>
 		</div>
 	);
---
+++ src/CategoryType.jsx
@@ -0,0 +1,13 @@
+const CategoryType = ({ handleCategory }) => {
+	return (
+		<select name='category' id='category-menu' onChange={handleCategory}>
+			<option value='all'>All</option>
+			<option value='type'>Type</option>
+			<option value='habitat'>Habitat</option>
+			<option value='gender'>Gender</option>
+			<option value='ability'>Ability</option>
+		</select>
+	);
+};
+
+export default CategoryType;
