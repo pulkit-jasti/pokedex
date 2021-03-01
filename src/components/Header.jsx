f303ca8bcadc4779cec666dd8db9acf0247f360d
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-01 21:19:39 +0530

    Header styling complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,23 +2,27 @@ const Header = ({ categoryHandler, optionsHandler, categoryOptionsList, searchHa
 	return (
 		<header>
 			<div>
-				<select name='category' id='category-menu' onChange={categoryHandler}>
-					<option value='all'>All</option>
-					<option value='type'>Type</option>
-					<option value='habitat'>Habitat</option>
-					<option value='ability'>Ability</option>
-					<option value='color'>Colour</option>
-					<option value='shape'>Shape</option>
-				</select>
-				<select name='category' id='category-menu' onChange={optionsHandler}>
-					{categoryOptionsList}
-				</select>
+				<div className='select select-1'>
+					<select name='category' id='category-menu' onChange={categoryHandler}>
+						<option value='all'>All</option>
+						<option value='type'>Type</option>
+						<option value='habitat'>Habitat</option>
+						<option value='ability'>Ability</option>
+						<option value='color'>Colour</option>
+						<option value='shape'>Shape</option>
+					</select>
+				</div>
+				<div className='select select-2'>
+					<select name='category' id='category-menu' onChange={optionsHandler}>
+						{categoryOptionsList}
+					</select>
+				</div>
 			</div>
 			<div className='title-container'>
 				<img src='https://cdn.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png' alt='' />
 			</div>
 			<form action='' onSubmit={searchHandler}>
-				<input type='text' onChange={searchInputHandler} />
+				<input type='text' placeholder='Enter the name of a pokemon' onChange={searchInputHandler} />
 				<button type='submit'>Search</button>
 			</form>
 		</header>
