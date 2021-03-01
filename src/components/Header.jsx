e7f41506c0363777fd5f67887344e0a7a9f7630f
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-01 18:11:03 +0530

    Basic header styling updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 const Header = ({ categoryHandler, optionsHandler, categoryOptionsList, searchHandler, searchInputHandler }) => {
 	return (
 		<header>
-			<select name='category' id='category-menu' onChange={categoryHandler}>
-				<option value='all'>All</option>
-				<option value='type'>Type</option>
-				<option value='habitat'>Habitat</option>
-				<option value='ability'>Ability</option>
-				<option value='color'>Colour</option>
-				<option value='shape'>Shape</option>
-			</select>
-			<select name='category' id='category-menu' onChange={optionsHandler}>
-				{categoryOptionsList}
-			</select>
+			<div>
+				<select name='category' id='category-menu' onChange={categoryHandler}>
+					<option value='all'>All</option>
+					<option value='type'>Type</option>
+					<option value='habitat'>Habitat</option>
+					<option value='ability'>Ability</option>
+					<option value='color'>Colour</option>
+					<option value='shape'>Shape</option>
+				</select>
+				<select name='category' id='category-menu' onChange={optionsHandler}>
+					{categoryOptionsList}
+				</select>
+			</div>
 			<div className='title-container'>
 				<img src='https://cdn.bulbagarden.net/upload/4/4b/Pok%C3%A9dex_logo.png' alt='' />
 			</div>
